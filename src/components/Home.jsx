509eb07f2067012b680381abaa76b8c5d341922e
nkrpradhan <nkrpradhan@gmail.com>
2022-10-31 14:53:42 +0000

    Display articles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
+import "../styles/Home.css";
+
 export default function Home() {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>NC News </h1>
+      <ul className="nav-bar">
+        <li>
+          <h1 className="nav-header">NC News</h1>
+        </li>
+        <li>
+          <h3>Coding</h3>
+        </li>
+        <li>
+          <h3>Football</h3>
+        </li>
+        <li>
+          <h3>Cooking</h3>
+        </li>
+      </ul>
     </>
   );
 }
